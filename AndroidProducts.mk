a92cf7d2e241bbb3f0df7b4212955f4bedb4ec67
chandu078 <chandudyavanapelli03@gmail.com>
2021-11-20 16:57:14 +0000

    lemonadep: Init Havoc-OS Based on Android 11
    Revert some unused commits:
    * Revert "lemonadep: Update FOD Soong config variable names"
      This reverts commit bb44617.
    
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_lemonadep.mk
+    $(LOCAL_DIR)/havoc_lemonadep.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_lemonadep-user \
-    lineage_lemonadep-userdebug \
-    lineage_lemonadep-eng
+    havoc_lemonadep-user \
+    havoc_lemonadep-userdebug \
+    havoc_lemonadep-eng
